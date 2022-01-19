4b7e57dfb07cf66ae09b7217bdb81348d397085b
Valentyn Kharasyk <lopanreka@gmail.com>
2022-01-19 20:27:43 +0200

    added router and navlink for navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -7,14 +8,18 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/* <Profile /> */}
-      <div class='app-wrapper-content'>
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div class='app-wrapper-content'>
+          <Routes>
+            <Route path='/dialogs' element={<Dialogs />}/>
+            <Route path='/profile' element={<Profile />}/>
+          </Routes>  
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
-console.log(s);
-// let s = {
-//     'nav': 'Navbar_nav__3ou9Q',
-//     'item': 'Navbar_item__3qaF3',
-//     'active' : 'Baksndakdn_actve'
- // }
-
-let c1 = "item";
-let c2 = "active";
-// "item active"
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${c2}`;
-
 
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}>
-            <a>Profile</a>
+        <div>
+            <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
         </div>
-        <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+        <div>
+            <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
         </div>
         <div className={s.item}>
             <a>News</a>
