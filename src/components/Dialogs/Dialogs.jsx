1931b4584f09797180218db2de1b220e1d026ae2
Valiantus <lopanreka@gmail.com>
2022-03-30 21:47:02 +0300

    dialogs and posts into map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,7 +16,7 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Max'},
         {id: 2, name: 'Andrew'},
         {id: 3, name: 'Greg'},
@@ -25,7 +25,7 @@ const Dialogs = (props) => {
         {id: 6, name: 'Wendy'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Morning!'},
@@ -33,15 +33,16 @@ const Dialogs = (props) => {
         {id: 5, message: 'Hi'}
     ]
 
+    let dialogsElement = dialogs.map( (d) => <DialogItem name={d.name} id={d.id}/> );
+    let messagesElement = messages.map( (m) => <Message message={m.message}/> );
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElement}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElement}
             </div>
         </div>
     )
